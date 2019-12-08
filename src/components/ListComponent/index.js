7952e10ea741573ae69c1d52e898089b82a64b2d
pupysheva <anime-manga@bk.ru>
2019-12-08 21:13:37 +0300

    Изменен url на http://api.localhost/get_items

--- src/components/ListComponent/index.js
+++ src/components/ListComponent/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import RenderList from "../RenderList";
 import Form from "../FormComponent";
 
-
 class ListComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -14,46 +13,54 @@ class ListComponent extends React.Component {
   }
 
   getlist = () => {
-      console.log("sadfsdaf");
-      fetch('http://localhost:8085/api/get_items')
-          .then(response => response.json())
-          .then((jsonData) => {
-                  this.setState({
-                      isLoaded: true,
-                      items: jsonData
-                  });
-              },
-              (error) => {
-                  this.setState({
-                      isLoaded: false,
-                      error
-                  });
-              })
+    console.log("sadfsdaf");
+    fetch("http://api.localhost/get_items")
+      .then(response => response.json())
+      .then(
+        jsonData => {
+          this.setState({
+            isLoaded: true,
+            items: jsonData
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: false,
+            error
+          });
+        }
+      );
   };
 
+  componentDidMount() {
+    this.getlist();
+  }
 
-
-    componentDidMount() {
-        this.getlist();
-    }
-
-  delete = (id) =>{
-      console.log("Delete", id);
-      var url = "http://localhost:8085/api/delete_item/" + id;
-      fetch(url);
-      const {items} = this.state;
-      this.setState({
-          items: items.filter((item, i) => {
-              return item.id !== id
-          }),
+  delete = id => {
+    console.log("Delete", id);
+    var url = "http://api.localhost/delete_item/" + id;
+    fetch(url);
+    const { items } = this.state;
+    this.setState({
+      items: items.filter((item, i) => {
+        return item.id !== id;
       })
+    });
   };
 
-    update = (comps) => {
-        console.log("Update", comps);
-        var url = "http://localhost:8085/api/update_item/"+comps.name+"/"+comps.type+"/"+comps.count+"/" +comps.price;
-        fetch(url);
-    };
+  update = comps => {
+    console.log("Update", comps);
+    var url =
+      "http://api.localhost/update_item/" +
+      comps.name +
+      "/" +
+      comps.type +
+      "/" +
+      comps.count +
+      "/" +
+      comps.price;
+    fetch(url);
+  };
   render() {
     const { error, isLoaded, items } = this.state;
     if (error) {
@@ -62,38 +69,36 @@ class ListComponent extends React.Component {
       return <div>Загружаем...</div>;
     } else {
       return (
-          <div>
-              <Form update={this.update}/>
-              <table className={"table table-hover"}>
-                  <thead>
-                  <Zagolovki/>
-                  </thead>
-                  <tbody>
-                  <RenderList items={this.state.items} deleteItem={this.delete}/>
-                  </tbody>
-              </table>
-          </div>
-      )
+        <div>
+          <Form update={this.update} />
+          <table className={"table table-hover"}>
+            <thead>
+              <Zagolovki />
+            </thead>
+            <tbody>
+              <RenderList items={this.state.items} deleteItem={this.delete} />
+            </tbody>
+          </table>
+        </div>
+      );
     }
   }
 }
 
 const Zagolovki = () => {
-  return(
-      <tr>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Type</th>
-        <th>Count</th>
-        <th>Price</th>
-          <th>#</th>
-      </tr>
-  )
+  return (
+    <tr>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Type</th>
+      <th>Count</th>
+      <th>Price</th>
+      <th>#</th>
+    </tr>
+  );
 };
 
 export default ListComponent;
-
-
 
 //
 // const SimpleList = names => (
